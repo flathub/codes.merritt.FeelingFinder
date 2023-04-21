58f478142d33e33ba969bc24afcba038d5117dea
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-04-21 16:40:49 -0400

    chore: update recipe files

---
+++ .devcontainer/build-flatpak.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# When run from the vscode dev container this will build a .flatpak of the app.
+
+set -e
+
+set -x
+
+
+projectId=codes.merritt.FeelingFinder
+
+
+git submodule update --remote --merge --init --recursive
+flatpak-builder --force-clean build-dir $projectId.yml --repo=repo
+flatpak build-bundle repo $projectId.flatpak $projectId
