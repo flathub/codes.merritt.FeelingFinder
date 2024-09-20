6234cb7bda88f2c33997a5fde5a52904d9feb723
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-09-20 10:49:22 -0400

    fix: build libayatana manually
    For some reason it stopped working, so now
    apparently we need to build all this junk
    manually.
    
    Building it like this requires Vala, which makes
    the GNOME SDK the better choice, so we're
    switching to that I guess.

--- .devcontainer/build-flatpak.sh
+++ .devcontainer/build-flatpak.sh
@@ -3,12 +3,13 @@
 # When run from the vscode dev container this will build a .flatpak of the app.
 
 set -e
-
 set -x
-
 
 projectId=codes.merritt.FeelingFinder
 
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --system -y --noninteractive org.gnome.Sdk/x86_64/47
+flatpak install --system -y --noninteractive org.gnome.Platform/x86_64/47
 
 flatpak-builder --force-clean --disable-cache --disable-rofiles-fuse build-dir $projectId.yml --repo=repo
 flatpak build-bundle repo $projectId.flatpak $projectId
